dafcb315b1cedb1eafecc52fd74f5a183b5eedb1
Leon Muigai <leonmuigaike@gmail.com>
2026-06-10 14:34:50 +0300

    Add Dog class with methods for checkup, birthday celebration, and age retrieval

--- lib/dog.py
+++ lib/dog.py
@@ -1,1 +1,19 @@
-# Dog model goes here
+class Dog:
+  def __init__(self, name, breed, age, last_checkup=None):
+    self.name = name
+    self.breed = breed
+    self.age = age
+    self.last_checkup = last_checkup
+    
+  def checkup(self, date):
+    print(f"Checking up with {self.name} on {date}")
+    self.last_checkup = date
+    
+  def birthday_celebration(self):
+    self.age += 1
+    print(f"(self.name) is turning {self.age}")
+    
+  def get_age(self):
+    return self.age
+  
+  
